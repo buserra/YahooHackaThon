428f180d2bd339e8343db7fccb73a003c6f9804e
BowenChan <BowenmChan@gmail.com>
2015-02-28 11:30:45 -0800

    Update Queue Add

--- src/queueAdd.java
+++ src/queueAdd.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 
 public class queueAdd {
 
-	Queue Queue1, Queue2, Queue3, Queue4, Queue5, Queue6, Queue7;
+	Queue<Node> Queue1, Queue2, Queue3, Queue4, Queue5, Queue6, Queue7;
 	
 	public queueAdd(Node queue)
 	{
@@ -11,31 +11,38 @@ public class queueAdd {
 		{
 			case "Media":
 			{
-				
+				Queue1.add(queue);
+				break;
 			}
 			case "Frontpage":
 			{
-				
+				Queue2.add(queue);
+				break;
 			}
 			case "Mail":
 			{
-				
+				Queue3.add(queue);
+				break;
 			}
 			case "Flickr":
 			{
-				
+				Queue4.add(queue);
+				break;
 			}
 			case "Search":
 			{
-				
+				Queue5.add(queue);
+				break;
 			}
 			case "Revenue":
 			{
-				
+				Queue6.add(queue);
+				break;
 			}
 			case "Messenger":
 			{
-				
+				Queue7.add(queue);
+				break;
 			}
 			default:
 			{
