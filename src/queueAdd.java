3f3a6a10ff8a8a14fcc16624313a030fcd030c9c
BowenChan <BowenmChan@gmail.com>
2015-02-28 11:23:14 -0800

    Queue Add Skeleton

---
+++ src/queueAdd.java
@@ -0,0 +1,47 @@
+import java.util.Queue;
+
+
+public class queueAdd {
+
+	Queue Queue1, Queue2, Queue3, Queue4, Queue5, Queue6, Queue7;
+	
+	public queueAdd(Node queue)
+	{
+		switch(queue.property)
+		{
+			case "Media":
+			{
+				
+			}
+			case "Frontpage":
+			{
+				
+			}
+			case "Mail":
+			{
+				
+			}
+			case "Flickr":
+			{
+				
+			}
+			case "Search":
+			{
+				
+			}
+			case "Revenue":
+			{
+				
+			}
+			case "Messenger":
+			{
+				
+			}
+			default:
+			{
+				
+			}
+		}
+	}
+	
+}
